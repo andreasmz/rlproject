fce5c53b86172d61b5d594ead190258a35aa7f78
andreasmz <174248270+andreasmz@users.noreply.github.com>
2025-07-01 15:51:36 +0200

    improved game code

--- andreas2048/game.py
+++ andreas2048/game.py
@@ -5,29 +5,29 @@ from matplotlib.axes import Axes
 from matplotlib.colors import ListedColormap
 from matplotlib.colors import BoundaryNorm
 from matplotlib.patches import Arrow, Rectangle
-from typing import Literal
+from typing import Literal, Callable
 
 
 class Action(Enum):
-    UP = (0, "y", (-1, 0), range(0,4), range(0,4))
-    DOWN = (1, "y", (1, 0),  range(3, -1, -1), range(0,4))
-    LEFT = (2, "x", (0,-1), range(0,4), range(0,4))
-    RIGHT = (3, "x", (0,1), range(0,4), range(3,-1,-1))
+    UP = (0, "y", (-1, 0))
+    DOWN = (1, "y", (1, 0))
+    LEFT = (2, "x", (0,-1))
+    RIGHT = (3, "x", (0,1))
     
     axis: Literal["x", "y"]
     dy: int
     dx: int
-    xrange: list[int]
-    yrange: list[int]
+    xrange: Callable[[int], list[int]]
+    yrange: Callable[[int], list[int]]
 
-    def __new__(cls, value, axis, dxy, yrange, xrange):
+    def __new__(cls, value, axis, dxy):
         obj = object.__new__(cls)
         obj._value_ = value
         obj.axis = axis
         obj.dy = dxy[0]
         obj.dx = dxy[1]
-        obj.yrange = [y for y in yrange]
-        obj.xrange = [x for x in xrange]
+        obj.yrange = lambda s: [y for y in (range(0,s) if dxy[0] < 0 else range(s-1,-1,-1))]
+        obj.xrange = lambda s: [y for y in (range(0,s) if dxy[1] < 0 else range(s-1,-1,-1))]
         return obj
 
 class Game:
@@ -48,33 +48,51 @@ class Game:
                        ])
     mpl_norm = BoundaryNorm(range(0,13), ncolors=mpl_cmap.N)
 
-    def __init__(self, shape:tuple[int, int] = (4,4), seed: int|None = None) -> None:
+    def __init__(self, shape:tuple[int, int] = (4,4), seed: int|None = None, save_random_state: bool = False) -> None:
         self.grid = np.zeros(shape=shape, dtype=np.uint8)
+        self.save_random_state = save_random_state
         self.score = 0
-        self.alive = True
+        self._alive = True
         self.rnd = np.random.default_rng(seed=seed)
         self.history: list[np.ndarray] = [np.stack((self.grid, np.zeros(shape=self.grid.shape, dtype=self.grid.dtype)), axis=0)]
         self.score_history = [self.score]
+        self._rnd_history = [self.rnd.bit_generator.state]
 
         self.xyt_to_idx = lambda y, x, t: y*self.grid.shape[0] + x + t*self.grid.size + 2
         self.idx_to_xyt = lambda idx: ( (int((idx-2) % self.grid.size) // self.grid.shape[0]), int((idx-2) % self.grid.shape[0]) , int((idx-2) // self.grid.size) )
 
         self.try_spawn()
         self.try_spawn()
+
+    @property
+    def move_count(self) -> int:
+        return len(self.history) - 1
+    
+    @property
+    def alive(self) -> bool:
+        return self._alive
+    
+    @property
+    def highest_tile(self) -> int:
+        return 2**int(np.max(self.grid))
+    
+    @property
+    def reward(self) -> int:
+        return self.score - self.score_history[-1]
         
     def try_spawn(self) -> bool:
         if not self.alive:
             return False
         empty_fields = np.argwhere(self.grid == 0)
         if len(empty_fields) == 0:
-            self.alive = False
+            self._alive = False
             return False
-        ij = empty_fields[np.random.randint(low=0, high=len(empty_fields))]
-        x = np.random.choice([1,2], p=[0.8, 0.2])
+        ij = empty_fields[self.rnd.integers(low=0, high=len(empty_fields))]
+        x = self.rnd.choice([1,2], p=[0.8, 0.2])
         self.grid[*ij] = x
         self.history[-1][1, *ij] = 1
         if len(self.get_moves()) == 0:
-            self.alive = False
+            self._alive = False
             return False
         return True
 
@@ -85,13 +103,15 @@ class Game:
         grid_history = np.zeros(shape=(2,*self.grid.shape), dtype=self.grid.dtype)
         grid_history[0,:,:] = self.grid
         self.score_history.append(int(self.score))
-        for i1, y1 in enumerate(action.yrange):
-            for j1, x1 in enumerate(action.xrange):
+        if self.save_random_state:
+            self._rnd_history.append(self.rnd.bit_generator.state)
+        for i1, y1 in enumerate(action.yrange(self.grid.shape[0])):
+            for j1, x1 in enumerate(action.xrange(self.grid.shape[1])):
                 if self.grid[y1,x1] == 0:
                     continue
                 if action.axis == "y":
                     y2 = None
-                    for yy in action.yrange[:i1][::-1]: # Go as far up or down as possible until hitting another block
+                    for yy in action.yrange(self.grid.shape[0])[:i1][::-1]: # Go as far up or down as possible until hitting another block
                         if self.grid[yy,x1] == 0:
                             y2 = yy
                             continue
@@ -112,7 +132,7 @@ class Game:
 
                 else:
                     x2 = None
-                    for xx in action.xrange[:j1][::-1]: # Go as far up or down as possible until hitting another block
+                    for xx in action.xrange(self.grid.shape[1])[:j1][::-1]: # Go as far up or down as possible until hitting another block
                         if self.grid[y1,xx] == 0:
                             x2 = xx
                             continue
@@ -154,9 +174,13 @@ class Game:
     def undo(self) -> bool:
         if len(self.history) <= 1:
             return False
+        if not self.alive:
+            self._alive = True
         
         self.score = self.score_history.pop()
         self.grid = self.history.pop()[0]
+        if len(self._rnd_history) > 1:
+            self.rnd.bit_generator.state = self._rnd_history.pop()
         return True
 
     
@@ -184,3 +208,6 @@ class Game:
                     y0, x0, t = self.idx_to_xyt(self.history[-1][1,y,x])
 
                     ax.add_patch(Arrow(x0, y0, (x-x0), (y-y0), color="red" if t == 1 else "blue", width=0.5, alpha=0.3))
+
+    def __repr__(self) -> str:
+        return f"<2048 Game{' (Ended)' if not self.alive else ''}: score {self.score}; {self.move_count} moves lead to {self.highest_tile} as highest tile>"
